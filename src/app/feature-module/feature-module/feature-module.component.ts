742d5508c63156a81154b95bf14a9e1e7221aa84
K. Alex Mills <k.alex.mills@gmail.com>
2018-09-14 14:19:51 -0500

    fix: turns out we actually did need those styles.

--- src/app/feature-module/feature-module/feature-module.component.ts
+++ src/app/feature-module/feature-module/feature-module.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-feature-module',
-  templateUrl: './feature-module.component.html',
-  styleUrls: ['./feature-module.component.css']
+  templateUrl: './feature-module.component.html'
 })
 export class FeatureModuleComponent implements OnInit {
 
